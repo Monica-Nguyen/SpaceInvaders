6d3c170c240b81138ad0875b28db1917745eadf6
smitra33 <60757186+smitra33@users.noreply.github.com>
2020-04-12 02:44:08 -0600

    Text-based added with this merge, files are reorganized into 3 packages

--- SpaceInvaders_Version3/src/textBased/gameObjects.java
+++ SpaceInvaders_Version3/src/textBased/gameObjects.java
@@ -3,16 +3,17 @@ import spaceInvadersLogic.movingObjs.*;
 
 public class gameObjects extends MovableObject{
 
+	Boolean isVisible;
 	public gameObjects(double xP, double yP, int h) {
 		super(xP, yP, h);
 	}
 	
 	public void remove() {
-		
+		isVisible = false;
 	}
 
 	public boolean onScreen() {
-		return imageView.isVisible();
+		return isVisible;
 	}
 	
 	//makes the object take damage
